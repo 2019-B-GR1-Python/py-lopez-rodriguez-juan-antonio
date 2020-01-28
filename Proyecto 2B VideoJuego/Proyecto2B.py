df71e80df3e85ae1700f3fed6218e7f67f614672
JUAN ANTONIO LOPEZ RODRIGUEZ <juan.lopez08@epn.edu.ec>
2020-01-28 08:53:58 -0500

    Scrapy Crawl
    Fybeca y csv

--- Proyecto 2B VideoJuego/Proyecto2B.py
+++ Proyecto 2B VideoJuego/Proyecto2B.py
@@ -11,23 +11,55 @@ Mi_imagen = pygame.transform.scale(Mi_imagen, (120,140))
 Imagen_fondo = pygame.image.load("Imagenes/Fondo_bosque_magico.png")
 
 posX = 200
-posY = 100 
+posY = 400
 
-velocidad = 10
+velocidad = 1
 #Color_fondo = (255,255,255)
-derecha = True
+derecha = True   # para movimineto automatico
+
+rectangulo = pygame.Rect(0,0,100,50)
+rectangulo_dos = pygame.Rect(300,300,100,50)
 
 while True:
     ventana.blit(Imagen_fondo, (0,0))
     #ventana.fill(Color_fondo)
     ventana.blit(Mi_imagen,(posX,posY))
+
+    pygame.draw.rect(ventana,(180,70,70), rectangulo_dos)
+    pygame.draw.rect(ventana,(180,70,70),rectangulo)
+    #mover rectangulo
+    rectangulo.left, rectangulo.top = pygame.mouse.get_pos()
+
+    # colision
+    if rectangulo.colliderect(rectangulo_dos):
+        velocidad = 0
+        print('Colision')
+
+
+
     for event in pygame.event.get():
     # cerrar pantalla
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
     
-    # Movimiento
+    # Movimiento automatico
+    if derecha == True:
+        if posX<900:
+            posX+=velocidad
+            rectangulo_dos.left = posX
+        else:
+            derecha=False
+    else:
+        if posX>1:
+            posX-=velocidad
+            rectangulo_dos.left = posX
+        else:
+            derecha=True
+
+    """
+
+    # Movimiento teclado
         elif event.type == pygame.KEYDOWN:
             if event.key==K_LEFT:
                 posX-=velocidad
@@ -38,11 +70,13 @@ while True:
                 print("Izq libre")
             elif event.key==K_RIGHT:
                 print("derecha libre")
-    
-    # Movimiento con mouse
+    """
+    """
+    #Movimiento con mouse
     posX,posY = pygame.mouse.get_pos()
     posX = posX - 50
     posY = posY - 50
+    """
 
     pygame.display.update()
 
